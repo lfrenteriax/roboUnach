26c2be2e508c4be37282efa166baa6020699a15e
lfrenteriax <lfrenteriax@hotmail.com>
2018-06-30 17:36:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 import os
 @app.route("/")
 def hello():
+    page = request.args.get('page', default = 1, type = int)
+    filter = request.args.get('filter', default = '*', type = str)
+    print filter
     return "Hello leo!"
+
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT',5000))
