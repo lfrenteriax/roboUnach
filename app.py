cb9912624e6f8977628858d2e8ab27cab560acb1
lfrenteriax <lfrenteriax@hotmail.com>
2018-06-30 17:32:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,10 @@
-from BaseHTTPServer import BaseHTTPRequestHandler
-import urlparse, json
+from flask import Flask
+app = Flask(__name__)
+import os
+@app.route("/")
+def hello():
+    return "Hello leo!"
 
-class GetHandler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        parsed_path = urlparse.urlparse(self.path)
-        message = '\n'.join([
-            'CLIENT VALUES:',
-            'client_address=%s (%s)' % (self.client_address,
-                self.address_string()),
-            'command=%s' % self.command,
-            'path=%s' % self.path,
-            'real path=%s' % parsed_path.path,
-            'query=%s' % parsed_path.query,
-            'request_version=%s' % self.request_version,
-            '',
-            'SERVER VALUES:',
-            'server_version=%s' % self.server_version,
-            'sys_version=%s' % self.sys_version,
-            'protocol_version=%s' % self.protocol_version,
-            '',
-            ])
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(message)
-        return
-
-    def do_POST(self):
-        content_len = int(self.headers.getheader('content-length'))
-        post_body = self.rfile.read(content_len)
-        self.send_response(200)
-        self.end_headers()
-
-        data = json.loads(post_body)
-
-        self.wfile.write(data['foo'])
-        return
-
-if __name__ == '__main__':
-    from BaseHTTPServer import HTTPServer
-    server = HTTPServer(('localhost', 5000), GetHandler)
-    print 'Starting server at http://localhost:8080'
-    server.serve_forever()
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port)
